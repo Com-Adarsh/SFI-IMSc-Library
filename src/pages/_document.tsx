9fd290777b3899b9bcbb1455c7085e210e9d2ada
Com-Adarsh <adarshjchandran617@gmail.com>
2026-04-25 13:54:05 +0530

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="theme-color" content="#D70A0A" />
+          <meta name="description" content="Abhimanyu Learning Space - Professional Digital Library for CUSAT Students" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
